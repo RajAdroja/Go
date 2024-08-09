2acea25d8c7a78281985248c32d3f32c2f87d33b
RajAdroja <rajadroja015@gmail.com>
2024-08-08 20:41:27 -0700

    Extracted some functionality and created greetingPrefix function

--- main.go
+++ main.go
@@ -12,18 +12,21 @@ func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	prefix := englishHelloPrefix
-	switch language {
-	case spanish:
-		prefix = spanishHelloPrefix
-
-	case french:
-		prefix = frenchHelloPrefix
-	}
-	return prefix + name
+	return greetingPrefix(language) + name
 
 }
 
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
 func main() {
-	fmt.Println(Hello("raj", "French"))
+	fmt.Println(Hello("raj", "Spanish"))
 }
